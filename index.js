97da4fa68502eadefdf85443e843bbbc2701f5ea
4dibbp90 <4dibbp90@solent.ac.uk>
2021-12-14 17:26:47 +0000

    coords to xyz entities

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 import 'aframe';
 import '@ar-js-org/ar.js';
-//import 'aframe-look-at-component';
-import {GoogleProjection} from 'jsfreemaplib';
+import 'aframe-look-at-component';
+import { GoogleProjection } from 'jsfreemaplib';
 
-AFRAME.registerComponent("dynamic-content", {
-    init: function()
-    {
+AFRAME.registerComponent("poi", {
+    init: function () {
         console.log('working');
         // google projection
         navigator.geolocation.getCurrentPosition(async (gpspos) => {
-        const location = new  GoogleProjection();
-        //long lat gps
-        console.log(`long:${gpspos.coords.longitude},lat:${gpspos.coords.latitude}`)
-        const lon = gpspos.coords.longitude
-        const lat = gpspos.coords.latitude
+            const location = new GoogleProjection();
+            //long lat gps
+            console.log(`long:${gpspos.coords.longitude},lat:${gpspos.coords.latitude}`)
+            const lon = gpspos.coords.longitude
+            const lat = gpspos.coords.latitude
 
-        const projected = location.project(lon, lat);
-        console.log(`Easting: ${projected[0]}, x: ${projected[0]}, northing: ${projected[1]}, z: ${-projected[1]}`);
+            const projected = location.project(lon, lat);
+            console.log(`Easting: ${projected[0]}, x: ${projected[0]}, northing: ${projected[1]}, z: ${-projected[1]}`);
 
-        
-        document.querySelector("[camera]").setAttribute("position", {
-            x: projected[0],
-            y: 0,
-            z: -projected[1]
-        });
-      
-        const west = gpspos.coords.longitude - 0.05
-        const south = gpspos.coords.latitude - 0.05
-        const east = gpspos.coords.longitude + 0.05
-        const north = gpspos.coords.latitude + 0.05
-        console.log(`west: ${west}, south: ${south}, east: ${east}, north: ${north}`);
 
-        const response = await fetch(`https://hikar.org/webapp/map/all?bbox=${west},${south},${east},${north}`)
-        const parsedJson = await response.json();
-        console.log('output')
-        console.log(parsedJson);
+            document.querySelector("[camera]").setAttribute("position", {
+                x: projected[0],
+                y: 0,
+                z: -projected[1]
+            });
+
+            const west = gpspos.coords.longitude - 0.05
+            const south = gpspos.coords.latitude - 0.05
+            const east = gpspos.coords.longitude + 0.05
+            const north = gpspos.coords.latitude + 0.05
+            console.log(`west: ${west}, south: ${south}, east: ${east}, north: ${north}`);
+
+            const response = await fetch(`https://hikar.org/webapp/map/all?bbox=${west},${south},${east},${north}`)
+            const parsedJson = await response.json();
+            console.log('output')
+            console.log(parsedJson);
             parsedJson.features.forEach(feature => {
-            
-            console.log(`name: ${feature.properties.name},${feature.geometry.coordinates}`);
-           
 
-        });  
-    
+                console.log(`name: ${feature.properties.name},${feature.geometry.coordinates}`);
+
+                const point = document.createElement('a-entity')
+                point.setAttribute('geometry', { primitive: 'box' });
+                point.setAttribute('material', { color: 'blue' });
+                const [entlat, entlon] = location.project(feature.geometry.coordinates[0], feature.geometry.coordinates[1])
+                point.setAttribute('position', {
+                    x: entlat,
+                    y: 0,
+                    z: -entlon
+                });
+                console.log(`entlat: ${entlat}, entlon: ${entlon}`);
+            });
 
 
 
-        });    
-}});
+
+        });
+    }
+});
