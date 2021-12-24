5d839d11cdc9624fbaac555c401b4651cc44d14d
4dibbp90 <4dibbp90@solent.ac.uk>
2021-12-24 11:03:09 +0000

    general clean up

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import 'three.js'
 
 AFRAME.registerComponent("poi", {
     init: function () {
-        console.log('working');
+        console.log('POI');
         this.loaded = false;
         window.addEventListener('gps-camera-update-position', e => {
             if (this.loaded === false) {
@@ -16,101 +16,105 @@ AFRAME.registerComponent("poi", {
         })
 
         // google projection
-        navigator.geolocation.watchPosition( 
-            
-        
-            async(gpspos) => {
-            console.log(`Lat ${gpspos.coords.latitude} Lon ${gpspos.coords.longitude}`)
-        
-            
-            document.getElementById('info').innerHTML = (`Current Location: Lat ${gpspos.coords.latitude} Lon ${gpspos.coords.longitude}`)
-            
-            
-            const location = new GoogleProjection();
-            //long lat gps
-            console.log(`long:${gpspos.coords.longitude},lat:${gpspos.coords.latitude}`)
-            const lon = gpspos.coords.longitude
-            const lat = gpspos.coords.latitude
-
-            const projected = location.project(lon, lat);
-            console.log(`Easting: ${projected[0]}, x: ${projected[0]}, northing: ${projected[1]}, z: ${-projected[1]}`);
 
 
-            document.querySelector("[camera]").setAttribute("position", {
-                x: projected[0],
-                y: 0,
-                z: -projected[1]
-            });
 
-            const west = gpspos.coords.longitude - 0.05
-            const south = gpspos.coords.latitude - 0.02
-            const east = gpspos.coords.longitude + 0.05
-            const north = gpspos.coords.latitude + 0.02
-            console.log(`west: ${west}, south: ${south}, east: ${east}, north: ${north}`);
+        navigator.geolocation.watchPosition(
 
-            const response = await fetch(`https://hikar.org/webapp/map/all?bbox=${west},${south},${east},${north}`)
-            const parsedJson = await response.json();
-            console.log('output')
-            console.log(parsedJson);
-            parsedJson.features.forEach(feature => {
-
-                console.log(`name: ${feature.properties.name},${feature.geometry.coordinates}`);
-
-                const point = document.createElement('a-entity')
-                const text = document.createElement('a-entity')
-                //TEST NO MODEL
-                //point.setAttribute('geometry', { primitive: 'box' });
-                //point.setAttribute('material', { color: 'blue' });
-                //POINTER MODEL
-
-                if (`${feature.properties.amenity}` === "cafe") {
-                    console.log("cafe")
-                    point.setAttribute('gltf-model', '#cafe');
-
-                }else if(`${feature.properties.amenity}` === "restaurant") {
-                    console.log("restaurant")
-                    point.setAttribute('gltf-model', '#restaurant');
-
-                }else if(`${feature.properties.amenity}` === "pub") {
-                    console.log("restaurant")
-                    point.setAttribute('gltf-model', '#pub');
+            async (gpspos) => {
+                console.log(`Lat ${gpspos.coords.latitude} Lon ${gpspos.coords.longitude}`)
                 
-                }else {
-                    console.log("other")
-                    point.setAttribute('gltf-model', '#pointer');
-                }
-                point.setAttribute('scale', { x: 50, y: 50, z: 50 })
-                //point.setAttribute('text', {value: 'point'});
-                text.setAttribute('text', { value: `${feature.properties.name}` })
-                /// text.setAttribute('look-at',{}) 
-                text.setAttribute('scale', { x: 100, y: 100, z: 100 });
+                document.getElementById('info').innerHTML = (`Current Location: Lat ${gpspos.coords.latitude} Lon ${gpspos.coords.longitude}`)
 
-                const [entlat, entlon] = location.project(feature.geometry.coordinates[0], feature.geometry.coordinates[1])
 
-                point.setAttribute('position', {
-                    x: entlat,
+                const location = new GoogleProjection();
+                //long lat gps
+                console.log(`long:${gpspos.coords.longitude},lat:${gpspos.coords.latitude}`)
+                const lon = gpspos.coords.longitude
+                const lat = gpspos.coords.latitude
+
+                const projected = location.project(lon, lat);
+                console.log(`Easting: ${projected[0]}, x: ${projected[0]}, northing: ${projected[1]}, z: ${-projected[1]}`);
+
+
+                document.querySelector("[camera]").setAttribute("position", {
+                    x: projected[0],
                     y: 0,
-                    z: -entlon
-                });
-                text.setAttribute('position', {
-                    x: entlat,
-                    y: 0.2,
-                    z: -entlon
+                    z: -projected[1]
                 });
 
+                const west = gpspos.coords.longitude - 0.05
+                const south = gpspos.coords.latitude - 0.02
+                const east = gpspos.coords.longitude + 0.05
+                const north = gpspos.coords.latitude + 0.02
+                console.log(`west: ${west}, south: ${south}, east: ${east}, north: ${north}`);
 
-                this.el.sceneEl.appendChild(point);
-                this.el.sceneEl.appendChild(text);
-                console.log(`entlat: ${entlat}, entlon: ${entlon}`);
-              });
+                const response = await fetch(`https://hikar.org/webapp/map/all?bbox=${west},${south},${east},${north}`)
+                const parsedJson = await response.json();
+                console.log('output')
+                console.log(parsedJson);
+
+                //forEach loop
+                parsedJson.features.forEach(feature => {
+
+                    console.log(`name: ${feature.properties.name},${feature.geometry.coordinates}`);
+
+                    const pointText = document.createElement('a-entity')
+                    const point = document.createElement('a-entity')
+                    const text = document.createElement('a-entity')
+                    //TEST NO MODEL
+                    //point.setAttribute('geometry', { primitive: 'box' });
+                    //point.setAttribute('material', { color: 'blue' });
+                    //POINTER MODEL
+
+                    if (`${feature.properties.amenity}` === "cafe") {
+                        console.log("cafe")
+                        point.setAttribute('gltf-model', '#cafe');
+
+                    } else if (`${feature.properties.amenity}` === "restaurant") {
+                        console.log("restaurant")
+                        point.setAttribute('gltf-model', '#restaurant');
+
+                    } else if (`${feature.properties.amenity}` === "pub") {
+                        console.log("restaurant")
+                        point.setAttribute('gltf-model', '#pub');
+
+                    } else {
+                        console.log("other")
+                        point.setAttribute('gltf-model', '#pointer');
+                    }
+                    point.setAttribute('scale', { x: 50, y: 50, z: 50 })
+                    text.setAttribute('text', { value: `${feature.properties.name}` })
+                    text.setAttribute('scale', { x: 100, y: 100, z: 100 });
+
+                    const [entlat, entlon] = location.project(feature.geometry.coordinates[0], feature.geometry.coordinates[1])
+
+                    point.setAttribute('position', {
+                        x: entlat,
+                        y: 0,
+                        z: -entlon
+                    });
+                    text.setAttribute('position', {
+                        x: entlat,
+                        y: 0.5,
+                        z: -entlon
+                    });
+
+
+                    pointText.appendChild(point);
+                    pointText.appendChild(text);
+                    this.el.sceneEl.appendChild(pointText);
+
+                    console.log(`entlat: ${entlat}, entlon: ${entlon}`);
+                });
 
 
 
 
-    //},
-    //{
-    //    enableHighAccuracy:true, 
-    //    maximumAge: 10000 
-    });
+                //},
+                //{
+                //    enableHighAccuracy:true, 
+                //    maximumAge: 10000 
+            });
     }
 });
